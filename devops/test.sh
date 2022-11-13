7276ff6cefe7459ee152e7aef0cb42f13f6c5d9e
Valery <valeronh@gmail.com>
2022-11-13 20:34:58 +0200

    request function added

--- devops/test.sh
+++ devops/test.sh
@@ -1,24 +1,26 @@
 #!/bin/bash -x
 
 rc=0
-request=$( curl -s http://localhost:5000/attendance | grep "Valery Khirin" | wc -l )
-request2=$( curl -s http://localhost:5000/about | grep "Valery Khirin" | wc -l )
-request3=$( curl -s http://localhost:5000/flow | grep "dockerization" | wc -l )
-request4=$( curl -s http://localhost:5000/home | grep "calculation" | wc -l )
 
-if [ $request -eq 0 ]; then
-        echo "Attendance test failed"
-        rc=1
-elif [ $request2 -eq 0 ]; then
-        echo "About test failed"
-        rc=1
-elif [ $request3 -eq 0 ]; then
-        echo "Flow test failed"
-        rc=1
-elif [ $request4 -eq 0 ]; then
-        echo "Home page test failed"
-        rc=1
-fi
+request() {
+	page="${1}"
+	word="${2}"
+	request=$( curl -s http://localhost:5000/${page} | grep "${word}" | wc -l )
+	if [ $request -eq 0 ]; then
+        	echo "${page} Test Failed"
+        	rc=1
+	fi	
+}
 
-exit ${rc}
+main() {
+	request "attendance" "Valery Khirin"
+	request "about" "Valery Khirin"
+	request "flow" "dockerization"
+	request "home" "Valery Khirin"
+
+	exit ${rc}
+}
+
+main
+
 
